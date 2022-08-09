bcfa0091b48272fcb4abfdf8a8e8aa8047bd744e
Shahribonu <shahribonuotaqulova01@gmail.com>
2022-08-09 22:56:05 +0500

    commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">All</li>
+        <li>Meat</li>
+        <li>Vegiterian</li>
+        <li>Grill</li>
+        <li>Sauce</li>
+        <li>Closed</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
